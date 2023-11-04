7aa216e7f2a9b8c6900841bbf9fd41a13680d7ee
SandrLite <mr.wlas@mail.ru>
2023-11-04 18:02:04 +0300

    HomeWork4

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,42 +1,45 @@
-﻿using HomeWork;
+﻿int[] arr =  { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-var father = new FamilyMember("Иван", Gender.Male, DateOnly.Parse("25.02.1988"));
-var mother = new FamilyMember("Ольга", Gender.Female, DateOnly.Parse("02.04.1989"));
-father.AddSpouse(mother);
-mother.AddSpouse(father);
+int target = 24;
 
-var child1 = new FamilyMember("Миша", Gender.Male, DateOnly.Parse("11.10.2018"));
-child1.SetParents(father, mother);
-father.AddChild(child1);
-mother.AddChild(child1);
+Console.WriteLine("Первый вариант:");
+#region First option
+var s = new HashSet<int>();
+foreach (var i in arr)
+{
+    var s1 = target - i;
 
-var child2 = new FamilyMember("Игорь", Gender.Male, DateOnly.Parse("12.05.2019"));
-child2.SetParents(father, mother);
-father.AddChild(child2);
-mother.AddChild(child2);
+    foreach (var j in arr)
+    {
+        var s2 = s1 - j;
 
+        if (s.Contains(s2))
+        {
+            Console.WriteLine($"{target} = {s2} + {j} + {i}");
+        }
+        else
+        {
+            s.Add(i);
+        }
+    }
+}
+#endregion
+Console.WriteLine("Второй вариант:");
 
-var father2 = new FamilyMember("Саша", Gender.Male, DateOnly.Parse("05.07.1982"));
-var mother2 = new FamilyMember("Света", Gender.Female, DateOnly.Parse("02.04.1986"));
-father2.AddSpouse(mother2);
-mother2.AddSpouse(father2);
+#region Second option
+var triplets = from a in arr
+               from b in arr
+               from c in arr
+               where a + b + c == target
+               select new { A = a, B = b, C = c };
 
-father.SetParents(father2, mother2);
-mother2.AddChild(father);
-father2.AddChild(father);
-
-mother = new FamilyMember("Ирина", Gender.Female, DateOnly.Parse("01.04.1981"));
-father.AddSpouse(mother);
-mother.AddSpouse(father);
-
-var child3 = new FamilyMember("Тимур", Gender.Male, DateOnly.Parse("11.10.2018"));
-child3.SetParents(father, mother);
-father.AddChild(child3);
-mother.AddChild(child3);
-
-var child4 = new FamilyMember("Алла", Gender.Female, DateOnly.Parse("01.04.2014"));
-child4.SetParents(father,mother);
-father.AddChild(child4);
-mother.AddChild(child4);
-
-FamilyMember.PrintTree(father2);
+if (triplets.Any())
+{
+    var firstTriplet = triplets.First();
+    Console.WriteLine($"{target} = {firstTriplet.A} + {firstTriplet.B} + {firstTriplet.C}");
+}
+else
+{
+    Console.WriteLine("Не найдено");
+}
+#endregion
