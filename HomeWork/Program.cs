6aabeadc14601340f7ddba3fd68649d302cb01e2
SandrLite <mr.wlas@mail.ru>
2023-11-29 11:44:27 +0300

    HomeWork8

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,100 +1,20 @@
-﻿using System.Reflection;
-using System.Text;
-
-namespace HomeWork
+﻿namespace HomeWork
 {
     class Program
     {
+        /*
+            Объедините две предыдущих работы (практические работы 2 и 3): поиск файла и поиск текста в файле написав 
+            утилиту которая ищет файлы определенного расширения с указанным текстом. Рекурсивно. Пример вызова утилиты: utility.exe txt текст.
+        */
         static void Main(string[] args)
         {
+            string directory = "222";
+            string fileName = "1.txt";
+            string pathFile = Path.Combine(directory, fileName);
+            string searchValue = "искомый_текст";
 
-            var v = CreateTestClassInstance(1, 11, "ssad", 3, new char[] { '4', '3', '8', '2' }, new char[] { 'f', 'g', 'k', 'i' });
-
-            string str = ObjectToString(v);
-            Console.WriteLine(str);
-            object str2 = StringToObject(str);
-        }
-        public static TestClass CreateTestClassInstance(int i, int ii, string s, decimal d, char[] c, char[] cc)
-        {
-            var testClassType = typeof(TestClass);
-            var testClass = Activator.CreateInstance(testClassType) as TestClass;
-
-            var testClassTwo = Activator.CreateInstance(testClassType, new object[] { i });
-
-            var testClassThird = Activator.CreateInstance(testClassType, new object[] { i, ii, s, d, c, cc });
-
-            return testClassThird as TestClass;
-        }
-        public static string ObjectToString(object obj)
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-            var type = obj.GetType();
-            stringBuilder.Append(type.ToString() + "\n");
-            stringBuilder.Append(type.Assembly + "\n");
-            stringBuilder.Append(type.Name + "\n");
-            var properties = type.GetProperties(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
-            foreach (var item in properties)
-            {
-                var value = item.GetValue(obj);
-
-                stringBuilder.Append(GetPropertyName(item) + ":");
-                if (item.PropertyType == typeof(char[]))
-                {
-                    stringBuilder.Append(new String(value as char[]) + "\n");
-                }
-                else
-                {
-                    stringBuilder.Append(value + "\n");
-                }
-            }
-
-            return stringBuilder.ToString();
-        }
-
-
-        private static string GetPropertyName(PropertyInfo property)
-        {
-            var customNameAttribute = (CustomNameAttribute)Attribute.GetCustomAttribute(property, typeof(CustomNameAttribute));
-            return customNameAttribute != null ? customNameAttribute.CustomFieldName : property.Name;
-        }
-
-
-        public static object StringToObject(string endString)
-        {
-            string[] str = endString.Split("\n");
-            var typeName = str[2];
-            var assemblyName = str[1];
-            var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName == assemblyName);
-            if (assembly != null)
-            {
-                var type = assembly.GetTypes().FirstOrDefault(t => t.FullName == typeName);
-                if (type != null)
-                {
-                    var obj = Activator.CreateInstance(type);
-                    var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-                    for (int i = 3; i < str.Length - 1; i++)
-                    {
-                        var propertyString = str[i].Split(":");
-                        var propertyName = propertyString[0];
-                        var propertyValue = propertyString[1];
-                        var property = properties.FirstOrDefault(p => GetPropertyName(p) == propertyName.Trim());  // Используем вспомогательный метод для получения имени поля
-                        if (property != null)
-                        {
-                            if (property.PropertyType == typeof(int))
-                            {
-                                property.SetValue(obj, int.Parse(propertyValue.Trim()));
-                            }
-                            else if (property.PropertyType == typeof(string))
-                            {
-                                property.SetValue(obj, propertyValue.Trim());
-                            }
-
-                        }
-                    }
-                    return obj;
-                }
-            }
-            return null;
-        }
+            SearcheFile.SearchFile(directory, fileName);
+            SearcheFile.SearchValue(pathFile, searchValue);
+        }        
     }
 }
